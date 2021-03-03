60531756c9562eb74f625860ef1fbe9dc6e8d871
Maxwell Cody <maxwell@cody.sh>
2021-03-03 14:09:12 -0600

    cleanup: Added cleanup macros for strings and addrinfo structs.
    To avoid compiler type-checking warnings, the _freestrp() function cannot take
    a void** and be used on strings. So a char** was used.

--- cleanup.h
+++ cleanup.h
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 static inline void _closep(int* fd) {
   if(*fd != -1) {
@@ -12,4 +15,18 @@ static inline void _closep(int* fd) {
   }
 }
 
+static inline void _freestrp(char** ptr) {
+  if(*ptr != NULL) {
+    free(*ptr);
+  }
+}
+
+static inline void _freeaddrinfop(struct addrinfo** ai) {
+  if(*ai != NULL) {
+    freeaddrinfo(*ai);
+  }
+}
+
 #define _cleanup_close_ __attribute__((cleanup(_closep)))
+#define _cleanup_freestr_ __attribute__((cleanup(_freestrp)))
+#define _cleanup_freeaddrinfo_ __attribute__((cleanup(_freeaddrinfop)))
