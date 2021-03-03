9e915c70bba55c63984a4e9d3f349431ebed151f
Maxwell Cody <maxwell@cody.sh>
2021-03-03 11:48:12 -0600

    mumblequery: Fixed a minor syntax error.

--- mumblequery.h
+++ mumblequery.h
@@ -13,7 +13,7 @@ struct mumble_query_reply {
   uint32_t users;
   uint32_t slots;
   uint32_t bandwidth;
-} __attribute__(packed);
+} __attribute__((packed));
 
 int mumble_query(const struct sockaddr_in*, uint64_t, struct mumble_query_reply*);
 int mumble_snversion(char*, size_t, union mumble_version);
