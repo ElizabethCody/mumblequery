546869481525e6ce47c3f76586f1c6c813382940
Maxwell Cody <maxwell@cody.sh>
2021-03-03 10:30:23 -0600

    mumblequery: Added reply struct to header.
    After correcting endianness, the struct _should_ be binary-compatible with the
    Mumble "UDP ping packet" response outlined on
    https://wiki.mumble.info/wiki/Protocol#UDP_Ping_packet.

--- mumblequery.h
+++ mumblequery.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <stdint.h>
+#include <netent/in.h>
+
+union mumble_version {
+  uint8_t version[4];
+};
+
+struct mumble_query_reply {
+  union mumble_version version;
+  uint32_t users;
+  uint32_t slots;
+  uint32_t bandwidth;
+} __attribute__(packed);
